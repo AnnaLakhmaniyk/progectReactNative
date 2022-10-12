6a2c1e6e803ef6e8b9edb5eb081994b29548c44f
AnnaLakhmaniyk <anya.21.volynets@gmail.com>
2022-10-12 11:34:41 +0300

    add app

--- App.js
+++ App.js
@@ -1,20 +1,56 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useCallback } from "react";
+import {
+  StyleSheet,
+  View,
+  TouchableWithoutFeedback,
+  Keyboard,
+  ImageBackground,
+} from "react-native";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import RegistrationScreen from "./component/Screens/RegistrationScreen";
+import LoginScreen from "./component/Screens/LoginScreen";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    "Poppins-Medium": require("./assets/fonts/Poppins-Medium.ttf"),
+    "Poppins-Regular": require("./assets/fonts/Poppins-Regular.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on open!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View style={styles.container} onLayout={onLayoutRootView}>
+        <ImageBackground
+          source={{ uri: "https://i.postimg.cc/d1MrrJNz/Photo-BG.png" }}
+          style={styles.image}
+        >
+          <RegistrationScreen />
+          {/* <LoginScreen/> */}
+        </ImageBackground>
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
+    backgroundColor: "#ecf0f1",
+  },
+  image: {
+    flex: 1,
+    resizeMode: "cover",
     justifyContent: "center",
+    // alignItems: "center",
   },
 });
